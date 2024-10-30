01bfb29b0cda9f74507d9484c1901a175a93a32e
¨rodolfodom¨ <rodolfodom7@gmail.com>
2024-10-30 12:52:35 -0600

    first commit

---
+++ graph.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 30 10:55:10 2024
+
+@author: rodolfodom
+"""
+
+linea_1 = {
+    "Pantitlán-1": {"Zaragoza": 1320, "Pantitlán-9": 469, "Pantitlán-A": 602, "Pantitlán-5": 250},
+    "Zaragoza": {"Gómez Farías": 762},
+    "Gómez Farías": {"Boulevard Puerto Aéreo": 595},
+    "Boulevard Puerto Aéreo": {"Balbuena": 595},
+    "Balbuena": {"Moctezuma": 703},
+    "Moctezuma": {"San Lázaro-1": 478},
+    "San Lázaro-1": {"Candelaria-1": 866, "San Lázaro-B": 300},
+    "Candelaria-1": {"Merced": 698, "Candelaria-4": 300},
+    "Merced": {"Pino Suárez-1": 745},
+    "Pino Suárez-1": {"Isabel la Católica": 382, "Pino Suárez-2": 300},
+    "Isabel la Católica": {"Salto del Agua-1": 445},
+    "Salto del Agua-1": {"Balderas-1": 458, "Salto del Agua-8":300},
+    "Balderas-1": {"Cuauhtémoc": 409, "Balderas-3":300},
+    "Cuauhtémoc": {"Insurgentes": 793},
+    "Insurgentes": {"Sevilla": 645},
+    "Sevilla": {"Chapultepec": 501},
+    "Chapultepec": {"Juanacatlán": 973},
+    "Juanacatlán": {"Tacubaya-1": 1158},
+    "Tacubaya-1": {"Observatorio": 1262, "Tacubaya-9": 300, "Tacubaya-7": 300},
+    "Observatorio": {}
+    }
+
+linea_2 = {
+    "Cuatro Caminos": {"Panteones": 1639},
+    "Panteones": {"Tacuba": 1416},
+    "Tacuba-2": {"Cuitláhuac": 637, "Tacuba-7": 300},
+    "Cuitláhuac": {"Popotla": 620},
+    "Popotla": {"Colegio Militar": 462},
+    "Colegio Militar": {"Normal": 516},
+    "Normal": {"San Cosme": 657},
+    
+    }
